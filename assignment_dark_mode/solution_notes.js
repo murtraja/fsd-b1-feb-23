be52f0477cd7a974894aa6fac92363fb8133119c
Murtaza Raja <murt.raja@gmail.com>
2023-10-20 23:18:57 +0530

    revision notes

---
+++ assignment_dark_mode/solution_notes.js
@@ -0,0 +1,37 @@
+function swapTheme() {
+  // requirement
+  // change div.day -> div.night
+  // change div.night -> div.day
+  // change button.button_day -> button.button_night
+  // change button.button_night -> button.button_day
+  // const div = document.getElementById("app")
+  // const button = document.getElementById("swap")
+  // div.classList.toggle('day')
+  // div.classList.toggle('night')
+  // button.classList.toggle('button_day')
+  // button.classList.toggle('button_night')
+  // if(div.classList.contains('day')) {
+  // div contains day
+  // day -> night
+  // div.classList.remove('day')
+  // div.classList.add('night')
+  // div.classList.replace('day', 'night')
+  // } else {
+  // div contains night
+  // night -> day
+  // div.classList.remove('night')
+  // div.classList.add('day')
+  // div.classList.replace('night', 'day')
+  // }
+  // if(button.classList.contains('button_day')) {
+  // button contains button_day
+  // button.classList.remove('button_day')
+  // button.classList.add('button_night')
+  // button.classList.replace('button_day', 'button_night')
+  // } else {
+  // button contains button_night
+  // button.classList.remove('button_night')
+  // button.classList.add('button_day')
+  // button.classList.replace('button_night', 'button_day')
+  // }
+}
