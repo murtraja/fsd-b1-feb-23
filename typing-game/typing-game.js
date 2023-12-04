ba5375e9c0608d4a3d4ee4238b8808aa96769fa3
Murtaza Raja <murt.raja@gmail.com>
2023-12-04 23:26:43 +0530

    adding spans for the quote

--- typing-game/typing-game.js
+++ typing-game/typing-game.js
@@ -3,3 +3,22 @@ const container = document.getElementById("container");
 const quoteDisplayElement = document.getElementById("quoteDisplay");
 const timerElement = document.getElementById("timer");
 const wpmElement = document.getElementById("wpm");
+
+async function getQuote() {
+  // returns a new quote from the API as string
+  const apiResult = await fetch(RANDOM_QUOTE_API_URL);
+  const jsonResult = await apiResult.json();
+  return jsonResult.content;
+}
+async function createSpansFromQuote(quote) {
+  for (let i = 0; i < quote.length; i++) {
+    const span = document.createElement("span");
+    span.innerHTML = quote[i];
+    quoteDisplayElement.appendChild(span);
+  }
+}
+async function initializeGame() {
+  const quote = await getQuote();
+  createSpansFromQuote(quote);
+}
+initializeGame();
