295b139d2e8a45d4916f74a92ba013f03fbad621
Murtaza Raja <murt.raja@gmail.com>
2023-12-04 22:02:59 +0530

    initial commit

---
+++ typing-game/typing-game.js
@@ -0,0 +1,5 @@
+const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+const container = document.getElementById("container");
+const quoteDisplayElement = document.getElementById("quoteDisplay");
+const timerElement = document.getElementById("timer");
+const wpmElement = document.getElementById("wpm");
